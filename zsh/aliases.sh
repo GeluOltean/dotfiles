f1113d41dd0054ee45bee880d7b1fd63e950647f
Gelu Oltean <geluoltean8@gmail.com>
2020-05-18 18:14:41 +0300

    Add zsh files

---
+++ zsh/aliases.sh
@@ -0,0 +1,17 @@
+# PROGRAM ALIASING
+alias py='python3'
+alias del='trash-put'
+
+# COMMAND ALIASING
+
+    # git 
+    alias gadd='git add'
+    alias gadda='git add .'
+    alias gstat='git status'
+    alias glast='git show'
+    alias gpush='git push'
+    alias gcomm='git commit --message'
+    
+    # services
+    alias apacheup='service apache2 start'
+    alias apachestop='service apache2 stop'

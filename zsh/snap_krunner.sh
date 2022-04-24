2e5033cebbd52c9723ade9b1fd0f7a08979125ae
Gelu Oltean <geluoltean8@gmail.com>
2022-04-24 19:54:31 +0300

    fix krunner by symlinking snap packages

---
+++ zsh/snap_krunner.sh
@@ -0,0 +1,20 @@
+shareApps="$HOME/.local/share/applications"
+snapApps="/var/lib/snapd/desktop/applications"
+
+# Detect all installed applications by snap
+for file in $snapApps/*.desktop
+do
+  # Get a not-ugly version of the .desktop 
+  # Example: todoist_todoist.desktop -> todoist.desktop
+  link="$shareApps/$(echo $file | cut -d '_' -f2)"
+
+  # Create new link if none exists
+  [[ -f $link ]] || ln -s $file $link
+done
+
+
+# NOTE: Remove this if you do NOT want automatic broken symbolic link cleanup
+for link in $shareApps/*.desktop; do
+  # Remove any broken *.desktop symbolic links
+  [[ -e $link ]] || rm $link
+done

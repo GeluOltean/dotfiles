d1e86b371c0dcacb41128bd6de3fb13750ecd4fc
Gelu Oltean <geluoltean8@gmail.com>
2023-01-29 20:28:07 +0200

    Remove theme styling from main config

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,11 +1,12 @@
 # General prompt configuration
         # shell options
-        export XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/snapd/desktop/"
         setopt histignorealldups sharehistory interactivecomments
-        HISTSIZE=2000
-        SAVEHIST=2000
-        HISTFILE=$HOME/.zsh_history
-	DOOMDIR=$DOTFILES/emacs/
+        export TERM="xterm-256color"
+        export XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/snapd/desktop/"
+        export HISTSIZE=2000
+        export SAVEHIST=2000
+        export HISTFILE=$HOME/.zsh_history
+	export DOOMDIR=$DOTFILES/emacs/
 
         # completion
         autoload -Uz compinit
@@ -15,7 +16,7 @@
         zstyle ':completion:*' format 'Completing %d'
         zstyle ':completion:*' group-name ''
         zstyle ':completion:*' menu select=2
-        eval "$(dircolors -b)"
+        # eval "$(dircolors -b)"
         zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
         zstyle ':completion:*' list-colors ''
         zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
@@ -26,22 +27,6 @@
         zstyle ':completion:*' verbose true
         zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
         zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
-
-# Theme configuration
-        export TERM="xterm-256color"
-
-        # prompt layout
-        POWERLEVEL9K_STATUS_VERBOSE=false
-        POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(virtualenv status vcs time)
-        POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator dir_joined)
-        POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-        POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""
-        POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="> "
-
-        # location
-        POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
-        POWERLEVEL9K_DIR_MAX_LENGTH=
-        POWERLEVEL9K_SHORTEN_DELIMITER=
 
 # Key bindings
         bindkey -e # force emacs bindings
